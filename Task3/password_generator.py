d2931ef0e06bbe7ad5ae1ba484322bdfd6106906
DHEERAJ REDDY <23eg106c61@anurag.edu.in>
2026-06-25 15:58:33 +0530

    Implement password generator functionality

--- Task3/password_generator.py
+++ Task3/password_generator.py
@@ -1,1 +1,14 @@
-
+import random
+import string
+print("===== Password Generator =====")
+length = int(input("Enter the desired password length: "))
+characters = (
+    string.ascii_letters +
+    string.digits +
+    string.punctuation
+)
+password = ""
+for i in range(length):
+    password += random.choice(characters)
+print("\nGenerated Password:")
+print(password)
